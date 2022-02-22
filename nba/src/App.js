73167220f3a64e65ec0636fb91d649f1a15695d5
ThomasMigeat <67231380+ThomasMigeat@users.noreply.github.com>
2022-02-22 15:53:52 +0100

    Récupération de l'Id de la Team

--- nba/src/App.js
+++ nba/src/App.js
@@ -6,17 +6,22 @@ class App extends Component {
     super(props)
     this.state = {
       playerName: null,
+      teamId: null,
       playerStats: {},
+      infoJoueur: {},
       gameStats: null
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+
     this.getPlayerId();
-    this.getGame();
+    this.getinfoJoueur();
+
     console.log(this.state.playerName);
     console.log(this.state.gameStats);
+    console.log(this.state.teamId)
 
   }
 
@@ -34,7 +39,7 @@ class App extends Component {
       .then(async res => {
         console.log(res.data.data);
         if(res.data.data[0] === undefined){
-          alert("il est bless ou n'a pas joue");
+          alert("il est blessé ou n'a pas joue");
         } else if(res.data.data.length > 1){
           alert("rentre le nom ou je vais m'enerver");
         } else{
@@ -45,6 +50,8 @@ class App extends Component {
         console.log(err)
       })
     }
+
+
   
     getPlayerStats = (playerId) =>{
       axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${playerId}`)
@@ -58,10 +65,23 @@ class App extends Component {
 
 
     getGame = () => {
-      axios.get(`https://www.balldontlie.io/api/v1/games?seasons[]=2021&team_ids[]=2`)
+      axios.get(`https://www.balldontlie.io/api/v1/games?seasons[]=2021&team_ids[]=${this.state.teamId}&per_page=50`)
       .then(async res=> {
         console.log(res.data.data)
         this.setState({ gameStats: res.data.data})
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+
+    getinfoJoueur = () => {
+      axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+      .then(async res=> {
+        console.log(res.data.data)
+        this.setState({ infoJoueur: res.data.data[0]})
+        this.setState({ teamId : res.data.data[0]["team"]?.id})
+        this.getGame();
+        //console.log("Team id "+res.data.data[0]["team"]?.id + " Ville team " + res.data.data[0]["team"]?.city)
       }).catch(err => {
         console.log(err)
       })
@@ -85,6 +105,12 @@ class App extends Component {
        </label>
        <input type="submit" value="Submit"/>
      </form>
+     <br />
+     Information joueur : {this.state.infoJoueur?.last_name} {this.state.infoJoueur?.first_name} {this.state.infoJoueur?.position}
+     <br />
+     Team id : {this.state.infoJoueur["team"]?.id}
+     <br />
+     <br />
      games played: {this.state.playerStats["games_played"]}
      <br />
      points averaged: {this.state.playerStats["pts"]}
